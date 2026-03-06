d1a615b1078342869ff6cac90ec6d58ef8190619
Gustavo Angelim <gustavoangelim@univar.local>
2026-03-06 15:55:19 -0300

    chore: setup initial project structure and spring dependencies

---
+++ src/main/java/com/angelim/itauTesteTecnico/ItauTesteTecnicoApplication.java
@@ -0,0 +1,13 @@
+package com.angelim.itauTesteTecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauTesteTecnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItauTesteTecnicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angelim/itauTesteTecnico/ItauTesteTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelim.itauTesteTecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauTesteTecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
